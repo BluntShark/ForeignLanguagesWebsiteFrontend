ef7ab08d991b92cc0ab0ce008f003b737107401d
Anastasia <klansis3000@mail.ru>
2024-05-19 15:11:30 +1000

    security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import WordsComponent from './components/Words/WordsComponent'
 import ListWordsComponent from './components/Words/ListWordsComponent'
 import { HomePage } from './components/HomePage'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
 
 function App() {
   return (
@@ -19,8 +21,14 @@ function App() {
           {/* //http://localhost:3000/words */}
           <Route path='/words' element = { <ListWordsComponent/> }></Route>
 
-          {/* //http://localhost:3000//add-word */}
+          {/* //http://localhost:3000/add-word */}
           <Route path='/add-word' element = { <WordsComponent /> }></Route>
+
+          {/* //http://localhost:3000/login */}
+          <Route path='/login' element={<Login />}></Route>
+
+          {/* //http://localhost:3000/register */}
+          <Route path='/register' element={<Register />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
