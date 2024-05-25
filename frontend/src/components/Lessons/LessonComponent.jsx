5d79a9e03731d8cfaf3c986f359da39732bb241c
Anastasia <klansis3000@mail.ru>
2024-05-25 15:16:48 +1000

    lesson

--- frontend/src/components/Lessons/LessonComponent.jsx
+++ frontend/src/components/Lessons/LessonComponent.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { createLesson } from '../../services/LessonService';
+import { useNavigate } from 'react-router-dom';
 
 const LessonComponent = () => {
 
@@ -8,6 +11,20 @@ const LessonComponent = () => {
   const [duration, setDuration] = useState('');
   const [difficultlyLevel, setDifficultlyLevel] = useState('');
   const [lessonCategory, setLessonCategory] = useState('');
+  const [difficultyLevels, setDifficultyLevels] = useState([]);
+  const [lessonCategories, setLessonCategories] = useState([]);
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/difficultlyLevels')
+      .then(response => setDifficultyLevels(response.data))
+      .catch(error => console.error('Error fetching difficulty levels:', error));
+
+    axios.get('http://localhost:8080/lessonCategories')
+      .then(response => setLessonCategories(response.data))
+      .catch(error => console.error('Error fetching lesson categories:', error));
+  }, []);
 
 
   function handleTitle(e) {
@@ -19,10 +36,29 @@ const LessonComponent = () => {
   function handleDuration(e) {
     setDuration(e.target.value);
   }
+  function handleDifficultlyLevel(e) {
+    const selectLevel = difficultyLevels.find(level => level.id == e.target.value);
+    console.log(selectLevel);
+    setDifficultlyLevel(selectLevel);
+  }
+  function handleLessonCategory(e) {
+    const selectCategory = lessonCategories.find(category => category.id == e.target.value)
+    console.log(selectCategory);
+    setLessonCategory(selectCategory);
+  }
+  function handleContent(e) {
+    setContent(e.target.value);
+  }
+
   function saveLesson(e){
     e.preventDefault();
-    const lesson = {title, dateOfCreation, duration}
+    const lesson = { title, content, dateOfCreation, duration, difficultlyLevel, lessonCategory }
     console.log(lesson);
+
+    createLesson(lesson).then((response) => {
+      console.log(response.data);
+      navigator('/lessons');
+    })
   }
 
   return (
@@ -34,7 +70,7 @@ const LessonComponent = () => {
           <div className='card-body'>
             <form>
 
-              <div className='form-group mb-2'>
+            <div className='form-group mb-2'>
                 <label className='form-label'> Название урока:</label>
                 <input
                 type='text'
@@ -43,6 +79,18 @@ const LessonComponent = () => {
                 value={title}
                 className='form-control'
                 onChange={handleTitle}
+                ></input>
+              </div>
+
+              <div className='form-group mb-2'>
+                <label className='form-label'> Контент:</label>
+                <input
+                type='text'
+                placeholder='Ввести название'
+                name='content'
+                value={content}
+                className='form-control'
+                onChange={handleContent}
                 ></input>
               </div>
 
@@ -70,29 +118,35 @@ const LessonComponent = () => {
                 ></input>
               </div>
 
-              {/* <div className='form-group mb-2'>
-                <label className='form-label'> Уровень сложости:</label>
-                <input
-                type='text'
-                placeholder='Ввести название'
-                name='title'
-                value={title}
-                className='form-control'
-                onChange={handleTitle}
-                ></input>
+              <div className='form-group mb-2'>
+                <label className='form-label'>Уровень сложности:</label>
+                <select
+                  name='difficultlyLevel'
+                  value={difficultlyLevel}
+                  className='form-control'
+                  onChange={handleDifficultlyLevel}
+                >
+                  <option value=''>Выберите уровень сложности</option>
+                  {difficultyLevels.map(level => (
+                    <option key={level.id} value={level.id}>{level.level}</option>
+                  ))}
+                </select>
               </div>
 
               <div className='form-group mb-2'>
-                <label className='form-label'> Категория урока:</label>
-                <input
-                type='text'
-                placeholder='Ввести название'
-                name='title'
-                value={title}
-                className='form-control'
-                onChange={handleTitle}
-                ></input>
-              </div> */}
+                <label className='form-label'>Категория урока:</label>
+                <select
+                  name='lessonCategory'
+                  value={lessonCategory}
+                  className='form-control'
+                  onChange={handleLessonCategory}
+                >
+                  <option value=''>Выберите категорию урока</option>
+                  {lessonCategories.map(category => (
+                    <option key={category.id} value={category.id}>{category.title}</option>
+                  ))}
+                </select>
+              </div>
 
               <button className='btn btn-success' onClick={saveLesson}> Отправить</button>
             </form>
